62dae099eb9cba7384955a2c9a61cbda2bfed51c
Pava Pai <pava@uw.edu>
2019-12-22 13:31:27 -0800

    changes-1 debug

--- index.js
+++ index.js
@@ -2,5 +2,8 @@ const express=require('express');
 const app=express();
 app.get('/',(req, res)=> {
     res.send({hi:'there'});
-})
-app.listen(5000)
+});
+
+cont PORT=process.env.PORT || 5000;
+
+app.listen(PORT);
